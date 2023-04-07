dc1efed00df751e3b6fb4c3a371fc149cf36142d
jairo-sierra2117 <jairoenriquesb@ufps.edu.co>
2023-04-06 23:00:59 -0500

    v 0.3

--- js/main.js
+++ js/main.js
@@ -1,71 +1,22 @@
 const navbar = document.querySelector(".navbar");
 const homebg = document.querySelector(".home__background");
-const menu = document.querySelector(".navbar__menu");
-const links = document.querySelector(".navbar__links");
 const sections = document.querySelectorAll("section");
-const menuIcon = document.querySelector('.navbar__menu-icon');
-
-
-
+const links = document.querySelectorAll(".nav-link");
+const menu = document.querySelector(".navbar-collapse");
 const config = {
   rootMargin: "0px",
   threshold: [0.6, 0.9],
 };
-
-function handleLlinks() {
-  if (window.innerWidth <= 991) {
-    links.classList.toggle("visible");
-  }
-}
-//menuIcon.addEventListener("click", handleLlinks);
-links.addEventListener("click", handleLlinks);
-
-menu.addEventListener("click", handleLlinks);
-links.addEventListener("click", handleLlinks);
-
-window.addEventListener("scroll", function () {
-  window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
-  window.scrollY < 100 && (navbar.style.background = `transparent`);
-});
-
-let observer = new IntersectionObserver(function (entries, self) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      intersectionHandler(entry);
-    }
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("show");
   });
-}, config);
-
-sections.forEach((section) => {
-  observer.observe(section);
 });
 
-function intersectionHandler(entry) {
-  const id = entry.target.id;
-  const currentlyActive = document.querySelector(".navbar__links  .active");
-  const shouldBeActive = document.querySelector(
-    ".navbar__links [data-ref=" + id + "]"
-  );
+$(function () {
+  $('body').scrollspy({ target: '#navbarNav', offset: 40 });
+});
 
-  if (currentlyActive) {
-    currentlyActive.classList.remove("active");
-  }
-  if (shouldBeActive) {
-    shouldBeActive.classList.add("active");
-  }
-}
-
-// window.addEventListener("mousemove", parallax);
-
-// function parallax(e) {
-//   elements.forEach((item) => {
-//     const speed = item.getAttribute("data-speed");
-//     console.log(speed);
-//     const x = (window.innerWidth - e.pageX * speed) / 100;
-//     const y = (window.innerHeight - e.pageY * speed) / 100;
-//     item.style.transform = `translateX(${x}px) translateY(${y}px)`;
-//   });
-// }
 
 ScrollReveal().reveal(".navbar", { delay: 250 });
 ScrollReveal().reveal(".home__profile", { delay: 350 });
@@ -79,7 +30,5 @@ ScrollReveal().reveal(".about__summary", { delay: 450 });
 ScrollReveal().reveal(".button--cta", { delay: 550 });
 ScrollReveal().reveal(".skill__title", { delay: 450 });
 ScrollReveal().reveal(".skill__item", { delay: 450 });
-ScrollReveal().reveal(".services__item", { delay: 450 });
-ScrollReveal().reveal(".portfolio__item", { delay: 450 });
-ScrollReveal().reveal(".contact__item", { delay: 450 });
-ScrollReveal().reveal(".footer", { delay: 450 });
+ScrollReveal().reveal(".contact__item", { delay: 250 });
+ScrollReveal().reveal(".footer", { delay: 250 });
